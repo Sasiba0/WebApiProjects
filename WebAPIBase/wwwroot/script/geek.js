3bd78b3360f02c99711cc0fefa621417ef22c23c
Sasiba0 <balintsasvari@gmail.com>
2023-11-29 13:24:06 +0100

    hf majdnem kesz

---
+++ WebAPIBase/wwwroot/script/geek.js
@@ -0,0 +1,23 @@
+﻿var viccek;
+window.onload = function Main()
+{
+    letoltes();
+}
+
+function letoltes()
+{
+    fetch("viccek.json")
+        .then(answer => answer.json())
+        .then(data => letoltesbef(data))
+}
+
+function letoltesbef(d) {
+    console.log("Sikeres letöltés");
+    console.log(d);
+    viccek = d;
+    for (var i = 0; i < viccek; i++)
+    {
+
+    }
+
+}
