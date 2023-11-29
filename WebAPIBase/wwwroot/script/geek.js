67a5c9ef7b31e99e4ce663a5ac373120edb79609
Sasiba0 <balintsasvari@gmail.com>
2023-11-29 16:25:01 +0100

    végleges

--- WebAPIBase/wwwroot/script/geek.js
+++ WebAPIBase/wwwroot/script/geek.js
@@ -6,8 +6,8 @@ window.onload = function Main()
 
 function letoltes()
 {
-    fetch("viccek.json")
-        .then(answer => answer.json())
+    fetch("jokes.json")
+        .then(response => response.json())
         .then(data => letoltesbef(data))
 }
 
@@ -15,9 +15,13 @@ function letoltesbef(d) {
     console.log("Sikeres letöltés");
     console.log(d);
     viccek = d;
-    for (var i = 0; i < viccek; i++)
-    {
-
+    for (var i = 0; i < d.length; i++) {
+        var kerdes = document.createElement("div");
+        kerdes.innerText = JSON.stringify(d[i]['question']);
+        var valasz = document.createElement("div");
+        valasz.innerText = JSON.stringify(d[i]['answer']);
+        document.body.appendChild(kerdes, valasz);
+        
     }
-
+    
 }
